21176e2e1ddf9cf7da1d81098c89ea92a6031abb
Felipe Rosa <flp.far@gmail.com>
2020-08-27 18:41:55 -0300

    Add company model

---
+++ app/models/company.rb
@@ -0,0 +1,15 @@
+class Company < ApplicationRecord
+  validates :name, :domain, presence: true
+  validates :domain, uniqueness: true
+  validate :domain_must_be_valid
+
+  private
+
+  def domain_must_be_valid
+    return if domain.blank? || domain.match(/^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}
+                                            [a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|
+                                            [a-z0-9-]{1,30}\.[a-z]{2,})$/x)
+
+    errors.add(:domain, 'não é válido')
+  end
+end
---
+++ spec/models/company_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  context 'validation' do
+    it 'attributes cant be blank' do
+      company = Company.new
+
+      company.valid?
+
+      expect(company.errors[:name]).to include('não pode ficar em branco')
+      expect(company.errors[:domain]).to include('não pode ficar em branco')
+    end
+
+    it 'domain is not valid if there are uppercase letters' do
+      company = Company.new(name: 'Coke', domain: 'Coke.com')
+
+      company.valid?
+
+      expect(company.errors[:domain]).to include('não é válido')
+    end
+
+    it 'domain is not valid if it doesnt match the domain requirements' do
+      company1 = Company.new(name: 'Coke', domain: 'coke.com.')
+      company2 = Company.new(name: 'Aqua', domain: '.aqua.com')
+      company3 = Company.new(name: 'Lime', domain: 'limecom')
+
+      company1.valid?
+      company2.valid?
+      company3.valid?
+
+      expect(company1.errors[:domain]).to include('não é válido')
+      expect(company2.errors[:domain]).to include('não é válido')
+      expect(company3.errors[:domain]).to include('não é válido')
+    end
+
+    it 'domain is valid' do
+      company1 = Company.new(name: 'Coke', domain: 'coke.com')
+      company2 = Company.new(name: 'Aqua', domain: 'aqua.com.br')
+      company3 = Company.new(name: 'Lime', domain: 'lim3.com')
+
+      company1.valid?
+      company2.valid?
+      company3.valid?
+
+      expect(company1.errors[:domain]).to be_empty
+      expect(company2.errors[:domain]).to be_empty
+      expect(company3.errors[:domain]).to be_empty
+    end
+
+    it 'domain must be unique' do
+      company1 = Company.create!(name: 'Coke', domain: 'coke.com')
+      company2 = Company.new(name: 'Aqua', domain: 'coke.com')
+
+      company2.valid?
+
+      expect(company2.errors[:domain]).to include('já está em uso')
+    end
+  end
+end
