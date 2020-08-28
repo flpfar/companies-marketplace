0d98d59efcbbb38ad0b86dc08552407fe2c09c52
Felipe Rosa <flp.far@gmail.com>
2020-08-28 13:45:00 -0300

    Add extra specs to user sign up

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -12,7 +12,28 @@ feature 'user sign up' do
     expect(page).to have_content('Bem vindo! Você realizou seu registro com sucesso')
   end
 
-  scenario 'doesnt fill password confirmation' do
+  scenario 'and is redirected to index page' do
+    visit root_path
+    click_on 'Criar uma conta'
+    fill_in 'Email', with: 'joao@coke.com.br'
+    fill_in 'Senha', with: '123123'
+    fill_in 'Confirme sua senha', with: '123123'
+    click_on 'Criar conta'
+
+    expect(current_path).to eq(root_path)
+  end
+
+  scenario 'and cant see index page on fail' do
+    visit root_path
+    click_on 'Criar uma conta'
+    fill_in 'Email', with: 'joao@coke.com.br'
+    fill_in 'Senha', with: '123123'
+    click_on 'Criar conta'
+
+    expect(current_path).not_to eq(root_path)
+  end
+
+  scenario 'and doesnt fill password confirmation' do
     visit root_path
     click_on 'Criar uma conta'
     fill_in 'Email', with: 'joao@coke.com.br'
@@ -23,7 +44,7 @@ feature 'user sign up' do
     expect(page).to have_content('Confirme sua senha não é igual a Senha')
   end
 
-  scenario 'email account is already in use' do
+  scenario 'and email account is already in use' do
     User.create!(email: 'joao@coke.com.br', password: '123456')
 
     visit root_path
