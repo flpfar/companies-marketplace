039b6c5c3000c06542bb7071e32f1e445fc89c74
Felipe Rosa <flp.far@gmail.com>
2020-08-31 10:03:31 -0300

    Fix view sale post specs with categories for post

--- spec/features/user_view_sale_posts_spec.rb
+++ spec/features/user_view_sale_posts_spec.rb
@@ -9,14 +9,16 @@ feature 'User views sale posts' do
   end
 
   scenario 'sucessfully' do
-    Company.create!(name: 'Coke', domain: 'coke.com.br')
+    coke_company = Company.create!(name: 'Coke', domain: 'coke.com.br')
+    eletro_category = Category.create!(name: 'Eletrodomésticos', company: coke_company)
     user_disabled = User.create!(email: 'maria@coke.com.br', password: '123123')
     user_bruno = User.create!(name: 'Bruno', social_name: 'Bruno', birth_date: '18/10/90', role: 'Gerente',
                               department: 'T.I', status: 1, email: 'bruno@coke.com.br', password: '123123')
-    sale_post_geladeira = SalePost.create!(title: 'Geladeira Brastemp', price: '800', user: user_bruno,
-                                           description: 'Geladeira semi nova, em ótimo estado')
+    sale_post_refrigerator = SalePost.create!(title: 'Geladeira Brastemp', price: '800', user: user_bruno,
+                                              description: 'Geladeira semi nova, em ótimo estado',
+                                              category: eletro_category)
     sale_post_fogao = SalePost.create!(title: 'Fogão Dako', price: '300', user: user_bruno,
-                                       description: 'Fogão ideal pra todos')
+                                       description: 'Fogão ideal pra todos', category: eletro_category)
 
     visit root_path
     fill_in 'Email', with: user_disabled.email
@@ -27,8 +29,8 @@ feature 'User views sale posts' do
       expect(page).to have_content(user_disabled.email)
     end
     within 'div.posts-container' do
-      expect(page).to have_content(sale_post_geladeira.title)
-      expect(page).to have_content("R$ #{sale_post_geladeira.price},00")
+      expect(page).to have_content(sale_post_refrigerator.title)
+      expect(page).to have_content("R$ #{sale_post_refrigerator.price},00")
       expect(page).to have_content(sale_post_fogao.title)
       expect(page).to have_content("R$ #{sale_post_fogao.price},00")
     end
@@ -47,8 +49,11 @@ feature 'User views sale posts' do
   end
 
   scenario 'only from the same company' do
-    Company.create!(name: 'Coke', domain: 'coke.com.br')
-    Company.create!(name: 'Pepsi', domain: 'pepsi.com.br')
+    coke_company = Company.create!(name: 'Coke', domain: 'coke.com.br')
+    pepsi_company = Company.create!(name: 'Pepsi', domain: 'pepsi.com.br')
+    eletro_coke_category = Category.create!(name: 'Eletrodomésticos', company: coke_company)
+    eletro_pepsi_category = Category.create!(name: 'Eletrodomésticos', company: pepsi_company)
+    furniture_pepsi_category = Category.create!(name: 'Móveis', company: pepsi_company)
     disabled_user = User.create!(email: 'maria@coke.com.br', password: '123123')
     user_bruno = User.create!(name: 'Bruno', social_name: 'Bruno',
                               role: 'Gerente', department: 'T.I', status: 1, birth_date: '18/10/90',
@@ -56,20 +61,20 @@ feature 'User views sale posts' do
     user_diego = User.create!(name: 'Diego', social_name: 'Diego',
                               role: 'Auxiliar', department: 'Comercial', status: 1, birth_date: '18/10/90',
                               email: 'diego@pepsi.com.br', password: '123123')
-    sale_post_geladeira = SalePost.create!(title: 'Geladeira Brastemp', price: '800', user: user_bruno,
+    sale_post_refrigerator = SalePost.create!(title: 'Geladeira Brastemp', price: '800', user: user_bruno,
                                            description: 'Geladeira semi nova, em ótimo estado',
-                                           company: user_bruno.company)
+                                           company: user_bruno.company, category: eletro_coke_category)
     sale_post_fogao = SalePost.create!(title: 'Fogão Dako', price: '300', user: user_diego,
-                                       description: 'Fogão ideal pra todos')
+                                       description: 'Fogão ideal pra todos', category: eletro_pepsi_category)
     sale_post_sofa = SalePost.create!(title: 'Sofá', price: '100', user: user_diego,
-                                      description: 'Sofá de 3 lugares')
+                                      description: 'Sofá de 3 lugares', category: furniture_pepsi_category)
 
     login_as disabled_user, scope: :user
     visit root_path
 
     within 'div.posts-container' do
-      expect(page).to have_content(sale_post_geladeira.title)
-      expect(page).to have_content("R$ #{sale_post_geladeira.price},00")
+      expect(page).to have_content(sale_post_refrigerator.title)
+      expect(page).to have_content("R$ #{sale_post_refrigerator.price},00")
       expect(page).not_to have_content(sale_post_fogao.title)
       expect(page).not_to have_content("R$ #{sale_post_fogao.price},00")
       expect(page).not_to have_content(sale_post_sofa.title)
@@ -78,7 +83,9 @@ feature 'User views sale posts' do
   end
 
   scenario 'from different users' do
-    Company.create!(name: 'Coke', domain: 'coke.com.br')
+    coke_company = Company.create!(name: 'Coke', domain: 'coke.com.br')
+    eletro_category = Category.create!(name: 'Eletrodomésticos', company: coke_company)
+    furniture_category = Category.create!(name: 'Móveis', company: coke_company)
     disabled_user = User.create!(email: 'maria@coke.com.br', password: '123123')
     user_bruno = User.create!(name: 'Bruno', social_name: 'Bruno', birth_date: '18/10/90',
                               role: 'Gerente', department: 'T.I', status: 1,
@@ -87,11 +94,12 @@ feature 'User views sale posts' do
                               role: 'Auxiliar', department: 'Comercial', status: 1,
                               email: 'diego@coke.com.br', password: '123123')
     sale_post_geladeira = SalePost.create!(title: 'Geladeira Brastemp', price: '800', user: user_bruno,
-                                           description: 'Geladeira semi nova, em ótimo estado')
+                                           description: 'Geladeira semi nova, em ótimo estado',
+                                           category: eletro_category)
     sale_post_fogao = SalePost.create!(title: 'Fogão Dako', price: '300', user: user_diego,
-                                       description: 'Fogão ideal pra todos')
+                                       description: 'Fogão ideal pra todos', category: eletro_category)
     sale_post_sofa = SalePost.create!(title: 'Sofá', price: '100', user: user_diego,
-                                      description: 'Sofá de 3 lugares')
+                                      description: 'Sofá de 3 lugares', category: furniture_category)
 
     login_as disabled_user, scope: :user
     visit root_path
