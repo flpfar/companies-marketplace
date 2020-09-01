e17a7861c6ea94b73c8ac80274e0fb7226f1b6b2
Felipe Rosa <flp.far@gmail.com>
2020-09-01 18:14:34 -0300

    Add buy button to post detail

--- spec/features/user_view_sale_post_details_spec.rb
+++ spec/features/user_view_sale_post_details_spec.rb
@@ -41,4 +41,52 @@ feature 'User views sale post details' do
       expect(page).to have_content("R$ #{sale_post_refrigerator.price},00")
     end
   end
+
+  context 'and buy button' do
+    scenario 'is available if user enabled and is not the owner' do
+      coke_company = Company.create!(name: 'Coke', domain: 'coke.com.br')
+      eletro_category = Category.create!(name: 'Eletrodomésticos', company: coke_company)
+      user_bruno = User.create!(name: 'Bruno', social_name: 'Bruno', birth_date: '18/10/90', role: 'Gerente',
+                                department: 'T.I', email: 'bruno@coke.com.br', password: '123123')
+      user_diego = User.create!(name: 'Diego', social_name: 'Diego', birth_date: '18/10/90',
+                                role: 'Auxiliar', department: 'Comercial',
+                                email: 'diego@coke.com.br', password: '123123')
+      post = SalePost.create!(title: 'Fogão Dako', price: '300', user: user_bruno,
+                              description: 'Fogão ideal pra todos', category: eletro_category)
+
+      login_as user_diego, scope: :user
+      visit sale_post_path(post.id)
+
+      expect(page).to have_link('Comprar')
+    end
+
+    scenario 'is not available if user not enabled' do
+      coke_company = Company.create!(name: 'Coke', domain: 'coke.com.br')
+      disabled_user = User.create!(email: 'maria@coke.com.br', password: '123123')
+      eletro_category = Category.create!(name: 'Eletrodomésticos', company: coke_company)
+      user_bruno = User.create!(name: 'Bruno', social_name: 'Bruno', birth_date: '18/10/90', role: 'Gerente',
+                                department: 'T.I', email: 'bruno@coke.com.br', password: '123123')
+      post = SalePost.create!(title: 'Fogão Dako', price: '300', user: user_bruno,
+                              description: 'Fogão ideal pra todos', category: eletro_category)
+
+      login_as disabled_user, scope: :user
+      visit sale_post_path(post.id)
+
+      expect(page).not_to have_link('Comprar')
+    end
+
+    scenario 'is not available if current_user is the owner' do
+      coke_company = Company.create!(name: 'Coke', domain: 'coke.com.br')
+      eletro_category = Category.create!(name: 'Eletrodomésticos', company: coke_company)
+      user_bruno = User.create!(name: 'Bruno', social_name: 'Bruno', birth_date: '18/10/90', role: 'Gerente',
+                                department: 'T.I', email: 'bruno@coke.com.br', password: '123123')
+      post = SalePost.create!(title: 'Fogão Dako', price: '300', user: user_bruno,
+                              description: 'Fogão ideal pra todos', category: eletro_category)
+
+      login_as user_bruno, scope: :user
+      visit sale_post_path(post.id)
+
+      expect(page).not_to have_link('Comprar')
+    end
+  end
 end
