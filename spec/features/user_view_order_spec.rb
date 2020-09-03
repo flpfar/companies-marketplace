3ca5bc4bf5eed90f13b4af9a3828705f26bb2f76
Felipe Rosa <flp.far@gmail.com>
2020-09-03 17:59:32 -0300

    Add user view order

--- spec/features/user_view_order_spec.rb
+++ spec/features/user_view_order_spec.rb
@@ -19,9 +19,44 @@ feature 'User views order' do
     expect(page).not_to have_content('Em andamento')
   end
 
-  xcontext 'as a seller'
-  xscenario 'has option to complete or cancel an order in progress'
-  xcontext 'as a buyer'
-  xscenario 'has no options to control the order'
-  xscenario 'can see messages'
+  context 'as a seller' do
+    scenario 'and has option to complete or cancel an order in progress' do
+      company = Company.create!(name: 'Coke', domain: 'coke.com')
+      user_seller = User.create!(name: 'Bruno', email: 'bruno@coke.com', password: '123123')
+      user_buyer = User.create!(name: 'Joao', email: 'joao@coke.com', password: '123123')
+      category = Category.create!(name: 'Eletrodomésticos', company: company)
+      post = SalePost.create!(title: 'Fogão novo', description: 'Em ótimo estado', price: 140,
+                              category: category, user: user_seller)
+      order = Order.create!(item_name: post.title, item_description: post.description, sale_post: post,
+                            posted_price: post.price, buyer: user_buyer, seller: user_seller)
+
+      login_as user_seller, scope: :user
+      visit order_path(order)
+
+      expect(page).to have_css('.seller-controllers')
+      within '.seller-controllers' do
+        expect(page).to have_link('Concluir venda')
+        expect(page).to have_link('Cancelar venda')
+      end
+    end
+  end
+  context 'as a buyer' do
+    scenario 'and has no options to control the order' do
+      company = Company.create!(name: 'Coke', domain: 'coke.com')
+      user_seller = User.create!(name: 'Bruno', email: 'bruno@coke.com', password: '123123')
+      user_buyer = User.create!(name: 'Joao', email: 'joao@coke.com', password: '123123')
+      category = Category.create!(name: 'Eletrodomésticos', company: company)
+      post = SalePost.create!(title: 'Fogão novo', description: 'Em ótimo estado', price: 140,
+                              category: category, user: user_seller)
+      order = Order.create!(item_name: post.title, item_description: post.description, sale_post: post,
+                            posted_price: post.price, buyer: user_buyer, seller: user_seller)
+
+      login_as user_buyer, scope: :user
+      visit order_path(order)
+
+      expect(page).to have_no_css('.seller-controllers')
+      expect(page).to have_no_link('Concluir venda')
+      expect(page).to have_no_link('Cancelar venda')
+    end
+  end
 end
