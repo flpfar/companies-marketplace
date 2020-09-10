aaac25027a54185282d5b2acbb99484d82cd243a
Felipe Rosa <flp.far@gmail.com>
2020-09-09 21:12:26 -0300

    Add user write message only on orders in progress

--- spec/features/user_write_messages_on_order_spec.rb
+++ spec/features/user_write_messages_on_order_spec.rb
@@ -45,4 +45,19 @@ feature 'User writes messages on order' do
       expect(page).to have_no_content('Buyer')
     end
   end
+
+  scenario 'is not available if order is completed' do
+    company = Company.create!(name: 'ABC', domain: 'abc.com')
+    games = company.categories.create!(name: 'Games')
+    seller = User.create!(name: 'Seller', email: 'seller@abc.com', password: '123123')
+    buyer = User.create!(name: 'Buyer', email: 'buyer@abc.com', password: '123123')
+    post = seller.sale_posts.create!(title: 'Playstation 4', description: 'Good as new', price: 900, category: games)
+    order = post.orders.create!(item_name: post.title, item_description: post.description, posted_price: post.price,
+                                buyer: buyer, seller: seller, status: :completed)
+
+    login_as buyer, scope: :user
+    visit order_path(order.id)
+
+    expect(page).to have_no_css('.messages-container')
+  end
 end
