d00af40be5295c1cf7ae11580342befd74b1c841
Felipe Rosa <flp.far@gmail.com>
2020-09-10 14:25:35 -0300

    Add date and sort notifications on my_account

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -1,6 +1,7 @@
 class MyAccountController < ApplicationController
   def show
     @user = current_user
+    @notifications_unseen = @user.notifications.unseen.order(created_at: :desc)
     @sale_orders = current_user.sale_orders.in_progress.includes([:buyer])
     @buy_orders = current_user.buy_orders.in_progress.includes([:seller, :buyer])
   end
