d68d322b9861af9d9eab4cdda913b63921cd9784
Felipe Rosa <flp.far@gmail.com>
2020-09-11 08:48:28 -0300

    Fix categories sorting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,6 @@ class HomeController < ApplicationController
     return redirect_to new_user_session_path unless user_signed_in?
 
     @posts = current_user.company.sale_posts.enabled.with_attached_cover.includes([:category]).order(created_at: :desc)
-    @categories = current_user.company.categories.sort
+    @categories = current_user.company.categories.order(name: :asc)
   end
 end
