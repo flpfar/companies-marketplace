7a8dc6fa54171ce496014f43de456ec140ab231a
Felipe Rosa <flp.far@gmail.com>
2020-09-15 19:24:04 -0300

    Add post author cant comment at his post

--- spec/features/user_ask_question_on_sale_post_spec.rb
+++ spec/features/user_ask_question_on_sale_post_spec.rb
@@ -25,4 +25,20 @@ feature 'User ask question on sale post' do
       expect(page).to have_content('Faz por 250?')
     end
   end
+
+  scenario 'unless user is the author of the post' do
+    coke_company = Company.create!(name: 'Coke', domain: 'coke.com.br')
+    eletro_category = Category.create!(name: 'Eletrodomésticos', company: coke_company)
+    user_bruno = User.create!(name: 'Bruno', birth_date: '18/10/90', role: 'Gerente',
+                              department: 'T.I', email: 'bruno@coke.com.br', password: '123123')
+    post = SalePost.create!(title: 'Fogão Dako', price: '300', user: user_bruno,
+                            description: 'Fogão ideal pra todos', category: eletro_category)
+
+    login_as user_bruno, scope: :user
+    visit sale_post_path(post.id)
+
+    within '.questions-container' do
+      expect(page).to have_no_css('.question-form')
+    end
+  end
 end
