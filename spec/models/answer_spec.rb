03e36f6eac2af9cc0cdd63aa6c97a08dad1a8c12
Felipe Rosa <flp.far@gmail.com>
2020-09-16 13:23:30 -0300

    Add validations to answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  context 'validation' do
+    it 'body cant be blank' do
+      company = Company.create!(name: 'Coshop', domain: 'coshop.com')
+      category = company.categories.create!(name: 'Games')
+      seller = User.create!(name: 'Seller', email: 'seller@coshop.com', password: '123123')
+      buyer = User.create!(name: 'Buyer', email: 'buyer@coshop.com', password: '123123')
+      post = seller.sale_posts.create!(title: 'Sale Post', description: 'my sale post', price: 123, category: category)
+      question = post.questions.create!(body: 'Is it new?', user: buyer)
+      answer = Answer.new(question: question)
+
+      answer.valid?
+
+      expect(answer.errors[:body]).to include('não pode ficar em branco')
+    end
+
+    it 'must be unique for a question' do
+      company = Company.create!(name: 'Coshop', domain: 'coshop.com')
+      category = company.categories.create!(name: 'Games')
+      seller = User.create!(name: 'Seller', email: 'seller@coshop.com', password: '123123')
+      buyer = User.create!(name: 'Buyer', email: 'buyer@coshop.com', password: '123123')
+      post = seller.sale_posts.create!(title: 'Sale Post', description: 'my sale post', price: 123, category: category)
+      question = post.questions.create!(body: 'Is it new?', user: buyer)
+      Answer.create!(body: 'Yes!', question: question)
+      answer = Answer.new(body: 'It depends.', question: question)
+
+      answer.valid?
+
+      expect(answer.errors[:question]).to include('já está em uso')
+    end
+  end
 end
